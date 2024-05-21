e4a53d47f6e94fbe4a5b354c8bc7fc601a23bd08
Suriyan Laohaprapanon <suriyant@gmail.com>
2024-05-21 19:14:40 +0200

    add model admin

--- softwarecite/front/admin.py
+++ softwarecite/front/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Package
+from .models import Package, Tag
 
 
 @admin.register(Package)
 class PackageAdmin(admin.ModelAdmin):
-    list_display = ('name', 'language', 'file_path', 'dataverse', 'repo_id')
-    search_fields = ('name', 'language', 'dataverse', 'repo_id')
+    list_display = ('name', 'language', 'file_path', 'dataverse', 'repo_id', 'tag')
+    search_fields = ('name', 'language', 'dataverse', 'repo_id', 'tag__name')
+    list_filter = ('tag',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at', 'updated_at')
+    search_fields = ('name', 'description')
